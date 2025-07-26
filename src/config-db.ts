a80700e634ebe280e6e4083e00bed4b91b2b049c
Joselivia <josephmutunga39@gmail.com>
2025-07-26 21:46:31 +0300

    ASsd

--- src/config-db.ts
+++ src/config-db.ts
@@ -2,17 +2,17 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-// export const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: false },
-// });
 export const pool = new Pool({
-  user: "postgres",
-  password: "@Joselivia254",
-  host: "localhost",
-  port: 5432,
-  database: "polling",
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+// export const pool = new Pool({
+//   user: "postgres",
+//   password: "@Joselivia254",
+//   host: "localhost",
+//   port: 5432,
+//   database: "polling",
+// });
 
 const createTables = async () => {
   const queries = [
